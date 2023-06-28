9e7e0d2bd93e300b35e65ff82c3b9ee57506ffe4
poojasharadshelke <113903221+poojasharadshelke@users.noreply.github.com>
2023-06-28 11:24:32 +0530

    Add files via upload

---
+++ Que-4.js
@@ -0,0 +1,14 @@
+//  comparison operations in JavaScript
+
+// Compare "7" and 7
+console.log("7" > 7);
+// Output: false
+
+// Compare "2" and "21"
+console.log("2" > "21");
+// Output: false
+
+// Compare "KL" and "S"
+console.log("KL" > "S");
+// Output: false
+
