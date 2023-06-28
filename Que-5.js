d3994608061c0a00a470bff645db4757db38f88a
poojasharadshelke <113903221+poojasharadshelke@users.noreply.github.com>
2023-06-28 11:24:45 +0530

    Add files via upload

---
+++ Que-5.js
@@ -0,0 +1,10 @@
+console.log(average(2, 1));
+
+// Define the average function
+function average(a, b) {
+  // Calculate the average by adding a and dividing the sum by 2
+  return (a + b) / 2;
+}
+
+// Call the average function with arguments 2 and 1
+console.log(average(2, 1));
